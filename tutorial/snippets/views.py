ae5945762111b341ff1b325522b0d9f84dec4454
Jonathan Sanchez <jonathansanchez595@gmail.com>
2018-12-20 10:39:39 -0300

    Se corrige permisos mal colocados en SnnnipetDetail view

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -10,7 +10,7 @@ from snippets.permissions import IsOwnerOrReadOnly
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -18,7 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 #Vistas genericas para ver Usuarios
 class UserList(generics.ListAPIView):
