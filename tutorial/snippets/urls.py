59237f5593f2fee6f5d30e1a23c33090ee97dfcf
Jonathan Sanchez <jonathansanchez595@gmail.com>
2018-12-26 10:46:30 -0300

    Finalizacion del tutorial, se agrega schema con core api

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls import include
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Pastebin API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -14,8 +18,10 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
+    path('schema/', schema_view),
 ]
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+
 ]
